a33d6fa3cf7c7e304cb47c9d7810d6d6f9a3d187
shirayuki <46ki75@gmail.com>
2024-03-18 23:39:17 +0900

    feat(graphiql): introduce GraphiQL component

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { invoke } from '@tauri-apps/api/tauri'
-import './App.scss'
+import React from 'react'
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState('')
-  const [name, setName] = useState('')
+// GraphiQL
+import { createGraphiQLFetcher } from '@graphiql/toolkit'
+import { GraphiQL } from 'graphiql'
+import 'graphiql/graphiql.css'
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke('greet', { name }))
-  }
+import styles from './App.module.scss'
 
+function App(): React.JSX.Element {
+  const fetcher = createGraphiQLFetcher({ url: 'http://localhost:3000' })
   return (
-    <div className='container'>
-      <h1>Welcome to Tauri!</h1>
-
-      <div className='row'>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo vite' alt='Vite logo' />
-        </a>
-        <a href='https://tauri.app' target='_blank' rel='noreferrer'>
-          <img src='/tauri.svg' className='logo tauri' alt='Tauri logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className='row'
-        onSubmit={(e) => {
-          e.preventDefault()
-          greet()
-        }}
-      >
-        <input
-          id='greet-input'
-          onChange={(e) => {
-            setName(e.currentTarget.value)
-          }}
-          placeholder='Enter a name...'
-        />
-        <button type='submit'>Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+    <div className={styles.container}>
+      <GraphiQL fetcher={fetcher} />
     </div>
   )
 }
