f5d4a99c96d5e5c4be6294d10640aba66fd6e7a1
shirayuki <46ki75@gmail.com>
2024-03-18 23:27:03 +0900

    feat(styles): introduce SCSS for styling enhancements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { invoke } from '@tauri-apps/api/tauri'
-import './App.css'
+import './App.scss'
 
 function App() {
   const [greetMsg, setGreetMsg] = useState('')
@@ -17,13 +17,13 @@ function App() {
       <h1>Welcome to Tauri!</h1>
 
       <div className='row'>
-        <a href='https://vitejs.dev' target='_blank'>
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
           <img src='/vite.svg' className='logo vite' alt='Vite logo' />
         </a>
-        <a href='https://tauri.app' target='_blank'>
+        <a href='https://tauri.app' target='_blank' rel='noreferrer'>
           <img src='/tauri.svg' className='logo tauri' alt='Tauri logo' />
         </a>
-        <a href='https://reactjs.org' target='_blank'>
+        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
@@ -39,7 +39,9 @@ function App() {
       >
         <input
           id='greet-input'
-          onChange={(e) => setName(e.currentTarget.value)}
+          onChange={(e) => {
+            setName(e.currentTarget.value)
+          }}
           placeholder='Enter a name...'
         />
         <button type='submit'>Greet</button>
